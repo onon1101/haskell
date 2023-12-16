583880b6fb2a55d3ca60caf1333d6f612b0f7e5b
onon1101 <mingzhuguly@gmail.com>
2023-12-16 18:38:42 +0800

    init

---
+++ Day01.hs
@@ -0,0 +1,9 @@
+module Main where
+
+doubleMe :: Int -> Int
+doubleMe result = result * 2
+
+main = do
+  let arr = [1, 2, 3, 4, 5]
+
+  print (arr)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# haskell
+
+拿來練手，僅此而已
+
+# Reference
+- [Functional Programming For Everyone系列](https://ithelp.ithome.com.tw/articles/10262566)
